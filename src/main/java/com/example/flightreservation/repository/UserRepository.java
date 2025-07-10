a425677347c42a2f170e71d4810c8749580496f8
nafisatou <nafisatouhamadou2@gmail.com>
2025-07-10 09:37:46 +0100

    fix: build failure

--- src/main/java/com/example/flightreservation/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.flightreservation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.flightreservation.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // Optionally: add custom queries here
-}
